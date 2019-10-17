f5d8dba038b765d665c2437e63d45e4328a3bf6f
unknown <dothithuyhangc12@gmail.com>
2019-10-17 16:47:01 +0700

    Update Chapter 2

---
+++ Chapter2/Chapter2/Chapter2/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chapter2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdStaticPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StaticPage());
+        }
+
+        private void CmdArrayPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ArrayPage());
+        }
+
+        private void CmdReferencePage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ReferencePage());
+        }
+
+        private void CmdConstructorsPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConstructorsPage());
+        }
+
+        private void CmdPropertyValuesPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PropertyValuesPage());
+        }
+    }
+}
---
+++ Chapter2/Chapter2/Chapter2/PropertyValuesPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PropertyValuesPage : ContentPage
+	{
+		public PropertyValuesPage ()
+		{
+			InitializeComponent ();
+		}
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
